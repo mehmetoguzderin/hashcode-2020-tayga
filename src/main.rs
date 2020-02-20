285c953afd71e4cd6058cc59d4aacac518abb213
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2020-02-20 21:04:31 +0300

    Include data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    let a_example = include_str!("data/a_example.txt");
+    let b_read_on = include_str!("data/b_read_on.txt");
+    let c_incunabula = include_str!("data/c_incunabula.txt");
+    let d_tough_choices = include_str!("data/d_tough_choices.txt");
+    let e_so_many_books = include_str!("data/e_so_many_books.txt");
+    let f_libraries_of_the_world = include_str!("data/f_libraries_of_the_world.txt");
     println!("Hello, world!");
 }
