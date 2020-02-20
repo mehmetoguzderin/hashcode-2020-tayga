b9c46bcaebc6c8411e16277527f75fde7dbec2b0
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2020-02-20 21:38:58 +0300

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,33 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if (args[1] == "a_example") {
+        // a_example
+        // cargo run --release -- a_example
         problem_name = String::from("a_example");
         problem = String::from(a_example);
     } else if (args[1] == "b_read_on") {
+        // b_read_on
+        // cargo run --release -- b_read_on
         problem_name = String::from("b_read_on");
         problem = String::from(b_read_on);
     } else if (args[1] == "c_incunabula") {
+        // c_incunabula
+        // cargo run --release -- c_incunabula
         problem_name = String::from("c_incunabula");
         problem = String::from(c_incunabula);
     } else if (args[1] == "d_tough_choices") {
+        // d_tough_choices
+        // cargo run --release -- d_tough_choices
         problem_name = String::from("d_tough_choices");
         problem = String::from(d_tough_choices);
     } else if (args[1] == "e_so_many_books") {
+        // e_so_many_books
+        // cargo run --release -- e_so_many_books
         problem_name = String::from("e_so_many_books");
         problem = String::from(e_so_many_books);
     } else if (args[1] == "f_libraries_of_the_world") {
+        // f_libraries_of_the_world
+        // cargo run --release -- f_libraries_of_the_world
         problem_name = String::from("f_libraries_of_the_world");
         problem = String::from(f_libraries_of_the_world);
     } else {
